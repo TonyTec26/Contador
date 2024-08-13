54e5ae136aba221e2e879a639c3df6a43fe564ce
Laion Pereira dos Santos <matheustec0405@gmail.com>
2024-08-13 11:28:02 -0300

    Primeiro commit

---
+++ MauiApp1/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿namespace MauiApp1
+{
+    public partial class MainPage : ContentPage {
+        int contador = 0;
+
+        public MainPage() {
+            InitializeComponent();
+        }
+
+        private void botaonegativo(object sender, EventArgs e) {
+            contador--;
+            ContadorLabel.Text = contador.ToString();
+
+        }
+
+        private void botaopositivo(object sender, EventArgs e) {
+            contador++;
+            ContadorLabel.Text = contador.ToString();
+        }
+    }
+
+}
